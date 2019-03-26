ae9c5ce5fd6b76bca740953c16dd9b8a42deb679
Aborysa <aborysa@gmail.com>
2019-03-26 11:31:45 +0100

    Moved assets to core

---
+++ core/src/com/tdt4240/game/assets/Assets.java
@@ -0,0 +1,13 @@
+package com.tdt4240.game.assets;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+
+public class Assets {
+  
+  private AssetManager assetManager;
+  public Assets(){
+    assetManager = new AssetManager();
+    
+  }
+}
